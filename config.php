07dd29a59f224c33c52cefb50120ffc4833d9e1c
akrabat <akrabat@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-04-14 20:01:30 +0000

    Dave Marshall's implentation of setting the base url
    git-svn-id: https://plugins.svn.wordpress.org/shorter-links/trunk@110599 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ config.php
@@ -0,0 +1,34 @@
+<?php
+// vim: set filetype=php expandtab tabstop=4 shiftwidth=4 autoindent smartindent:
+
+/**
+ * @package Shorter Links
+ * @author Dave Marshall (davemastergeneral@gmail.com)
+ * @license New BSD: http://akrabat.com/license/new-bsd
+ * @version 1.1
+ */
+
+if (isset($_POST['akrabat_sl_base_url'])) {
+    check_admin_referer('akrabat_sl_form');
+    update_option('akrabat_sl_base_url', $_POST['akrabat_sl_base_url']);
+?>
+    <div class="updated"><p><strong><?php _e('Options saved.' ); ?></strong></p></div>
+<?php
+}
+?>
+<div class="wrap">  
+    <h2>Shorter Links options</h2>
+      
+    <form name="akrabat_sl_form" method="post" action="<?php echo str_replace('%7E', '~', $_SERVER['REQUEST_URI']); ?>">  
+        <?php wp_nonce_field('akrabat_sl_form'); ?>
+        <p>
+           <label for="akrabat_sl_base_url">Base URL:</label>
+           <input type="text" id="akrabat_sl_base_url" name="akrabat_sl_base_url" 
+                   value="<?php echo get_option('akrabat_sl_base_url');?>" /> e.g. http://akrabat.com
+        </p>
+        <p class="submit">  
+            <input type="submit" name="Submit" value="Save" />
+        </p>  
+    </form>  
+</div>  
+
